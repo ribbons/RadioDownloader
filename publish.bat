b1c55684a7ccd46fba6d81ead091bc44f764f9b5
Matt Robinson <git@nerdoftheherd.com>
2007-05-15 20:09:39 +0000

    Created a batch file to do a clean and then full compile of the solution in release mode.

---
+++ publish.bat
@@ -0,0 +1,10 @@
+@echo off
+
+rem Batch file to publish the Radio Downloader and BBCLA plugin to the 'publish' folder.
+
+call "C:\Program Files\Microsoft Visual Studio 8\SDK\v2.0\Bin\sdkvars.bat"
+
+msbuild /t:Clean
+msbuild /p:Configuration=Release
+
+pause
