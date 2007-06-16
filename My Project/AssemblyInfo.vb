5873f91e1f59fd91f5a6658d7e855eb2d83b3e64
Matt Robinson <git@nerdoftheherd.com>
2007-06-16 16:34:46 +0000

    Version number and installer for 0.3.3 release.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -14,6 +14,6 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyTrademark("")>
 <Assembly: AssemblyCulture("")>
 
-<Assembly: AssemblyVersion("0.3.2.*")> 
+<Assembly: AssemblyVersion("0.3.3.*")> 
 
 
