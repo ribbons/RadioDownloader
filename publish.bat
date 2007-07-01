00a839e631afaa83f7c2b0f6e0a847cdebe20e00
Matt Robinson <git@nerdoftheherd.com>
2007-07-01 17:12:48 +0000

    Updated the Radio Downloader project file, so that the release build generates an optimised binary, but also creates pdb files as well.  These are needed to make the error reports useful, by including line numbers.

--- publish.bat
+++ publish.bat
@@ -1,6 +1,6 @@
 @echo off
 
-rem Batch file to publish the Radio Downloader and BBCLA plugin to the 'publish' folder.
+rem Batch file to publish the Radio Downloader and BBCLA plugin to the 'bin' folder.
 
 call "C:\Program Files\Microsoft Visual Studio 8\SDK\v2.0\Bin\sdkvars.bat"
 
