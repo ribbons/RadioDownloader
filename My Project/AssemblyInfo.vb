11587357e5a255ac945c5a1f3df0f71c2b5a81cc
Matt Robinson <git@nerdoftheherd.com>
2007-10-10 19:39:46 +0000

    Version number changes for 0.4.0 release.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -14,6 +14,6 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyTrademark("")>
 <Assembly: AssemblyCulture("")>
 
-<Assembly: AssemblyVersion("0.3.6.*")> 
+<Assembly: AssemblyVersion("0.4.0.*")> 
 
 
