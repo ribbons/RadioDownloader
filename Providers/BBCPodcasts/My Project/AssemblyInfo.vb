58125cf180f31a8ea4fc2be9d4ef6f706857f073
Matt Robinson <git@nerdoftheherd.com>
2008-01-04 21:49:25 +0000

    Version number changes for 0.4.7 release.

--- Providers/BBCPodcasts/My Project/AssemblyInfo.vb
+++ Providers/BBCPodcasts/My Project/AssemblyInfo.vb
@@ -31,4 +31,4 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.4.6.*")> 
+<Assembly: AssemblyVersion("0.4.7.*")> 
