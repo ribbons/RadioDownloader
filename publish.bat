6926a3641662e8f5d9f8e0fb0d72010a892a2727
Matt Robinson <git@nerdoftheherd.com>
2008-03-14 14:39:31 +0000

    Updated the publish batch file to work with version 3.5 of the .net framework tools. Added the new solution cache file to the ignore list for subversion, and excluded it from the zipped source.

--- publish.bat
+++ publish.bat
@@ -1,8 +1,8 @@
 @echo off
 
-rem Batch file to publish the Radio Downloader and BBCLA plugin to the 'bin' folder.
+rem Batch file to publish the Radio Downloader and plugins to the 'bin' folder.
 
-call "C:\Program Files\Microsoft Visual Studio 8\SDK\v2.0\Bin\sdkvars.bat"
+call "C:\Program Files\Microsoft SDKs\Windows\v6.1\Bin\setenv.cmd"
 
 msbuild /t:Clean
 msbuild /p:Configuration=Release
--- zip source.bat
+++ zip source.bat
@@ -7,7 +7,7 @@ rem create the temp folder if it doesn't already exist
 if not exist %tempfolder% mkdir %tempfolder%
 
 rem copy all of the files across that are needed, but try and miss out the files that can be easily re-created.
-robocopy .\ %tempfolder% /MIR /XD .svn obj /XF "Radio Downloader.exe" "Radio Downloader.exe.config" "Radio Downloader.pdb" "Radio Downloader.vshost.exe" "Radio Downloader.vshost.exe.config" "Radio Downloader.xml" System.Data.SQLite.DLL System.Data.SQLite.xml "Radio Downloader.suo" "Radio Donloader.msi" "Radio Downloader.wixobj" "Radio Downloader.log" "*.vbproj.user"
+robocopy .\ %tempfolder% /MIR /XD .svn obj /XF "Radio Downloader.exe" "Radio Downloader.exe.config" "Radio Downloader.pdb" "Radio Downloader.vshost.exe" "Radio Downloader.vshost.exe.config" "Radio Downloader.xml" System.Data.SQLite.DLL System.Data.SQLite.xml "Radio Downloader.sln.cache" "Radio Downloader.suo" "Radio Downloader.msi" "Radio Downloader.wixobj" "Radio Downloader.log" "*.vbproj.user"
 
 rem make sure that 7za.exe is on the PATH.
 7za a "%userprofile%\Desktop\Radio Downloader Source.7z" %tempfolder%
