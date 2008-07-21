187c7c319c8d49202dc98adcce91fc297e8d618f
Matt Robinson <git@nerdoftheherd.com>
2008-07-21 16:21:49 +0000

    Updated the name of the podcast provider assembly to PodcastProvider.

--- Providers/PodcastProvider/My Project/Settings.Designer.vb
+++ Providers/PodcastProvider/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.GeneralPodcasts.My.MySettings
+        Friend ReadOnly Property Settings() As Global.PodcastProvider.My.MySettings
             Get
-                Return Global.GeneralPodcasts.My.MySettings.Default
+                Return Global.PodcastProvider.My.MySettings.Default
             End Get
         End Property
     End Module
