429214acef32d433fc0b454e3df3b23f97a63449
Matt Robinson <git@nerdoftheherd.com>
2009-03-25 21:44:19 +0000

    Updated the zip source batch file to create a zip instead of 7zip file, as this is more widely compatible.

--- zip source.bat
+++ zip source.bat
@@ -10,6 +10,6 @@ rem copy all of the files across that are needed, but try and miss out the files
 robocopy .\ %tempfolder% /MIR /XD .svn obj /XF PodcastProvider.dll PodcastProvider.dll.config PodcastProvider.pdb PodcastProvider.xml "Radio Downloader.exe" "Radio Downloader.exe.config" "Radio Downloader.pdb" "Radio Downloader.vshost.exe" "Radio Downloader.vshost.exe.config" "Radio Downloader.vshost.exe.manifest" "Radio Downloader.xml" System.Data.SQLite.DLL System.Data.SQLite.xml "Radio Downloader.sln.cache" "Radio Downloader.suo" "Radio Downloader.msi" "Radio Downloader.wixobj" "Radio Downloader.log" "*.vbproj.user"
 
 rem make sure that 7za.exe is on the PATH.
-7za a "%userprofile%\Desktop\Radio Downloader Source.7z" %tempfolder%
+7za a -tzip "%userprofile%\Desktop\Radio Downloader Source.zip" %tempfolder%
 
 rmdir /S /Q %tempfolder%
