b2c0edb1361db49ba4a1282ac6c23a6cf40643be
Matt Robinson <git@nerdoftheherd.com>
2009-05-19 20:00:02 +0000

    Moved the code to catch unhandled exceptions and the /exit switch from the main form into ApplicationEvents where it *really* belongs.  Switched to using the standard application events for startup, startup next instance, and unhandledexception.  Changed from using Application.ThreadException to AppDomain.UnhandledException, as this seems to work better in this context.  Now also only add the extra exception handler when a debugger is not attached, as this makes debugging more straightforward.

---
+++ My Project/ApplicationEvents.vb
@@ -0,0 +1,71 @@
+' Utility to automatically download radio programmes, using a plugin framework for provider specific implementation.
+' Copyright © 2007-2009 Matt Robinson
+'
+' This program is free software; you can redistribute it and/or modify it under the terms of the GNU General
+' Public License as published by the Free Software Foundation; either version 2 of the License, or (at your
+' option) any later version.
+'
+' This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+' implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+' License for more details.
+'
+' You should have received a copy of the GNU General Public License along with this program; if not, write
+' to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Namespace My
+    Partial Friend Class MyApplication
+        Private Sub MyApplication_Startup(ByVal sender As Object, ByVal e As Microsoft.VisualBasic.ApplicationServices.StartupEventArgs) Handles Me.Startup
+            ' Add an extra handler to catch unhandled exceptions in other threads
+            If Debugger.IsAttached = False Then
+                AddHandler AppDomain.CurrentDomain.UnhandledException, AddressOf AppDomainExceptionHandler
+            End If
+
+            ' If /exit was passed on the command line, then just exit immediately
+            For Each commandLineArg As String In Environment.GetCommandLineArgs
+                If commandLineArg.ToLower = "/exit" Then
+                    e.Cancel = True
+                    Exit Sub
+                End If
+            Next
+        End Sub
+
+        Private Sub MyApplication_StartupNextInstance(ByVal sender As Object, ByVal e As Microsoft.VisualBasic.ApplicationServices.StartupNextInstanceEventArgs) Handles Me.StartupNextInstance
+            For Each commandLineArg As String In e.CommandLine
+                If commandLineArg.ToLower = "/exit" Then
+                    ' Close the application
+                    RadioDld.Main.mnuTrayExit_Click(sender, e)
+                    Exit Sub
+                End If
+            Next
+
+            ' Do the same as a double click on the tray icon
+            Call RadioDld.Main.mnuTrayShow_Click(sender, e)
+        End Sub
+
+        Private Sub MyApplication_UnhandledException(ByVal sender As Object, ByVal e As Microsoft.VisualBasic.ApplicationServices.UnhandledExceptionEventArgs) Handles Me.UnhandledException
+            If ReportError.Visible = False Then
+                Dim clsReport As New ErrorReporting(e.Exception)
+                ReportError.AssignReport(clsReport)
+                ReportError.ShowDialog()
+            End If
+        End Sub
+
+        Private Sub AppDomainExceptionHandler(ByVal sender As Object, ByVal e As System.UnhandledExceptionEventArgs)
+            Dim unhandledExp As Exception
+
+            Try
+                unhandledExp = DirectCast(e.ExceptionObject, Exception)
+            Catch classCastExp As InvalidCastException
+                ' The ExceptionObject isn't a child of System.Exception, so we don't know
+                ' how to report it.  Instead, let the standard .net dialog appear.
+                Exit Sub
+            End Try
+
+            If ReportError.Visible = False Then
+                Dim clsReport As New ErrorReporting(unhandledExp)
+                ReportError.AssignReport(clsReport)
+                ReportError.ShowDialog()
+            End If
+        End Sub
+    End Class
+End Namespace
