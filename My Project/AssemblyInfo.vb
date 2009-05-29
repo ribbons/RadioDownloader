a35577fc4e69721eed101f0000aad26197f73fba
Matt Robinson <git@nerdoftheherd.com>
2009-05-29 20:24:44 +0000

    Set the NeutralResourcesLanguage of the application and providers to en-GB, and tidied up the AssemblyInfo files to remove the boilerplate comments which are no longer relevant.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -1,21 +1,16 @@
+Imports System
 Imports System.Reflection
-Imports System.Runtime.CompilerServices
+Imports System.Resources
 Imports System.Runtime.InteropServices
-
-' General Information about an assembly is controlled through the following
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
 
 <Assembly: AssemblyTitle("Radio Downloader")> 
 <Assembly: AssemblyDescription("Utility to automatically download radio programmes made available online, using a plugin framework for provider specific implementation.")> 
 <Assembly: AssemblyCompany("www.nerdoftheherd.com")> 
 <Assembly: AssemblyProduct("Radio Downloader")> 
 <Assembly: AssemblyCopyright("Copyright © 2007-2009 Matt Robinson")> 
-<Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
-
-<Assembly: CLSCompliant(True)> 
-
+<Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyCulture("")> 
 <Assembly: AssemblyVersion("0.5.6.0")> 
 
-
+<Assembly: CLSCompliant(True)> 
+<Assembly: NeutralResourcesLanguageAttribute("en-GB")> 
--- Providers/PodcastProvider/My Project/AssemblyInfo.vb
+++ Providers/PodcastProvider/My Project/AssemblyInfo.vb
@@ -1,12 +1,7 @@
 Imports System
 Imports System.Reflection
+Imports System.Resources
 Imports System.Runtime.InteropServices
-
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
-
-' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("Podcast Plugin")> 
 <Assembly: AssemblyDescription("Plugin to support downloading podcasts within Radio Downloader.")> 
@@ -14,22 +9,8 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyProduct("Podcast Plugin")> 
 <Assembly: AssemblyCopyright("Copyright © 2007-2009 Matt Robinson")> 
 <Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyVersion("0.5.6.0")> 
 
 <Assembly: CLSCompliant(True)> 
-<Assembly: ComVisible(False)>
-
-'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("4e965b6e-f491-408b-a553-e2d14a2ffe46")> 
-
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version 
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
-
-<Assembly: AssemblyVersion("0.5.6.0")> 
+<Assembly: ComVisible(False)> 
+<Assembly: NeutralResourcesLanguageAttribute("en-GB")> 
