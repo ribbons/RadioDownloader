a2db765e330e6a440d49af9d3bf51f10e52881b0
Matt Robinson <git@nerdoftheherd.com>
2009-11-03 18:54:24 +0000

    Added a quick check for the existence of the Windows 7 SDK into the publish batch file.

--- publish.bat
+++ publish.bat
@@ -2,12 +2,15 @@
 
 rem Batch file to publish Radio Downloader and the plugins to the 'bin' folder.
 
+set sdklocation=%programfiles%\Microsoft SDKs\Windows\v7.0
+if not exist "%sdklocation%" goto nosdk
+
 rem Required to run the SDK setenv script
 setlocal ENABLEEXTENSIONS
 setlocal ENABLEDELAYEDEXPANSION
 
 rem Set up an x86 Release build environment
-call "%programfiles%\Microsoft SDKs\Windows\v7.0\Bin\setenv.cmd" /Release /x86
+call "%sdklocation%\Bin\setenv.cmd" /Release /x86
 if ERRORLEVEL 1 goto failed
 
 msbuild /t:Clean
@@ -15,6 +18,13 @@ if ERRORLEVEL 1 goto failed
 
 msbuild
 if ERRORLEVEL 1 goto failed
+
+goto exit
+
+:nosdk
+
+echo The Microsoft Windows SDK for Windows 7 does not appear to be installed
+echo Please install it and then try running this script again
 
 goto exit
 
