a41cc53667dd0e8a67c7711f41101657410b6682
Matt Robinson <git@nerdoftheherd.com>
2010-02-16 10:34:54 +0000

    Version number changes for the 0.9 beta release.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -10,7 +10,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyCopyright("Copyright © 2007-2010 Matt Robinson")> 
 <Assembly: AssemblyTrademark("")> 
 <Assembly: AssemblyCulture("")> 
-<Assembly: AssemblyVersion("0.8.4.0")> 
+<Assembly: AssemblyVersion("0.9.0.0")> 
 
 <Assembly: CLSCompliant(True)> 
 <Assembly: ComVisible(False)> 
