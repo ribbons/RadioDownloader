4a6640c454326377ff1cfd7273f52baccb7a6d9a
Matt Robinson <git@nerdoftheherd.com>
2010-08-29 11:55:22 +0000

    Changed the SearchBox control to be completely custom instead of inheriting from TextBox.  Instead, this uses a child TextBox for the actual text, with the theme set to SearchBoxEditComposited, and then draws the Windows Explorer SearchBox class backgrounds for the control.  This means that uxtheme handles all the tricky drawing text on glass, and partial transparency.

--- Controls/SearchBox.vb
+++ Controls/SearchBox.vb
@@ -15,16 +15,115 @@
 Option Strict On
 Option Explicit On
 
+Imports System.ComponentModel
+Imports System.Runtime.InteropServices
+Imports System.Windows.Forms.VisualStyles
+
 Public Class SearchBox
-    Inherits TextBox
+    Inherits Control
+
+    <DllImport("uxtheme.dll", SetLastError:=True, CharSet:=CharSet.Unicode)> _
+    Private Shared Function SetWindowTheme(ByVal hWnd As IntPtr, ByVal pszSubAppName As String, ByVal pszSubIdList As String) As Integer
+    End Function
+
+    ' Search box theme class, part and states
+    Private Const SEARCHBOX As String = "SearchBox"
+    Private Const SBBACKGROUND As Integer = 1
+
+    Private Const SBB_NORMAL As Integer = 1
+    Private Const SBB_HOT As Integer = 2
+    Private Const SBB_DISABLED As Integer = 3
+    Private Const SBB_FOCUSED As Integer = 4
+
+    Private boxState As Integer = 1
+    Private themeHeight As Integer
+
+    Private WithEvents textBox As TextBox
 
     Public Sub New()
         MyBase.New()
+
+        ' Create the child textbox control for the user to type in, without a border
+        textBox = New TextBox
+        textBox.BorderStyle = BorderStyle.None
+        Me.Controls.Add(textBox)
+
+        ' Work out the height that the search box should be displayed with
+        Dim sizeStyle As New VisualStyleRenderer(SEARCHBOX, SBBACKGROUND, SBB_NORMAL)
+
+        Using sizeGraphics As Graphics = Me.CreateGraphics
+            Me.themeHeight = sizeStyle.GetPartSize(sizeGraphics, ThemeSizeType.True).Height
+        End Using
     End Sub
 
-    Public Overrides Function GetPreferredSize(ByVal proposedSize As Size) As Size
-        ' Change the behavour of GetPreferredSize, as this allows AutoSize of the parent to work smoothly
-        Return New Size(Me.Width, Me.Height)
-    End Function
+    Private Sub SearchBox_HandleCreated(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.HandleCreated
+        If OsUtils.WinXpOrLater() Then
+            ' Set the theme of this parent control and the edit control, so they are rendered correctly
+            If SetWindowTheme(Me.Handle, "SearchBoxComposited", Nothing) <> 0 Then
+                Throw New Win32Exception
+            End If
+
+            If SetWindowTheme(textBox.Handle, "SearchBoxEditComposited", Nothing) <> 0 Then
+                Throw New Win32Exception
+            End If
+        End If
+    End Sub
+
+    Private Sub SearchBox_Paint(ByVal sender As Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Me.Paint
+        ' Clear the background of the control to make sure it is transparent when on glass
+        e.Graphics.Clear(Color.Transparent)
+
+        If VisualStyleRenderer.IsSupported Then
+            ' Paint the correct background for the control based on the current state
+            Dim searchBoxStyle As New VisualStyleRenderer(SEARCHBOX, SBBACKGROUND, boxState)
+            searchBoxStyle.DrawBackground(e.Graphics, New Rectangle(0, 0, Me.Width, Me.Height))
+        End If
+    End Sub
+
+    Private Sub textBox_MouseEnter(ByVal sender As Object, ByVal e As System.EventArgs) Handles textBox.MouseEnter
+        If boxState = SBB_NORMAL Then
+            boxState = SBB_HOT
+        End If
+
+        ' Repaint the control and child textbox
+        Me.Invalidate()
+        textBox.Invalidate()
+    End Sub
+
+    Private Sub textBox_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles textBox.MouseLeave
+        If boxState = SBB_HOT Then
+            boxState = SBB_NORMAL
+        End If
+
+        ' Repaint the control and child textbox
+        Me.Invalidate()
+        textBox.Invalidate()
+    End Sub
+
+    Private Sub textBox_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles textBox.GotFocus
+        boxState = SBB_FOCUSED
+        Me.Invalidate() ' Repaint the control
+    End Sub
+
+    Private Sub textBox_LostFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles textBox.LostFocus
+        boxState = SBB_NORMAL
+        Me.Invalidate() ' Repaint the control
+    End Sub
+
+    Private Sub SearchBox_Resize(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Resize
+        If Me.Height <> Me.themeHeight Then
+            ' Force the height to always be set to that specified from the theme
+            Me.Height = Me.themeHeight
+        End If
+
+        textBox.Top = 4
+        textBox.Left = 2
+        textBox.Width = Me.Width - (textBox.Left + 2)
+    End Sub
+
+    Private Sub textBox_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles textBox.TextChanged
+        ' Hook up changes to the child textbox through this control
+        Me.Text = textBox.Text
+    End Sub
 End Class
 
