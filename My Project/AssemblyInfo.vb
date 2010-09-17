6050fc7b132e83de6feacae30916bf67803585f8
Matt Robinson <git@nerdoftheherd.com>
2010-09-17 15:32:11 +0000

    Version number changes for the 0.17.1 beta release.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -11,7 +11,7 @@ Imports System.Security
 <Assembly: AssemblyCopyright("Copyright © 2007-2010 Matt Robinson")> 
 <Assembly: AssemblyTrademark("")> 
 <Assembly: AssemblyCulture("")> 
-<Assembly: AssemblyVersion("0.17.0.0")> 
+<Assembly: AssemblyVersion("0.17.1.0")> 
 
 <Assembly: CLSCompliant(True)> 
 <Assembly: ComVisible(False)> 
