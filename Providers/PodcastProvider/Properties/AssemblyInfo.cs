659844fc7a55b4446a29e50eb2a6a4124b2409dc
Matt Robinson <git@nerdoftheherd.com>
2012-02-11 20:16:57 +0000

    Version number changes for the 0.23 beta release.

--- Providers/PodcastProvider/Properties/AssemblyInfo.cs
+++ Providers/PodcastProvider/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Security;
 [assembly: AssemblyCopyright("Copyright © 2007-2012 Matt Robinson")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("0.22.0.0")]
+[assembly: AssemblyVersion("0.23.0.0")]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguageAttribute("en-GB")]
