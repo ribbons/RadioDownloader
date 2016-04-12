23fa0c5a01937e821b73b645e9a3cc1d15ec860f
Boris Ockham <Boris-Ockham@users.noreply.github.com>
2016-04-12 13:31:33 +0200

    Removed space and brackets from filename suffix
    Currently the brackets break alphabetical sorting by filename if two
    downloads have indentical publication times and titles in a podcast
    feed (e.g. episodes split into multiple parts).
    
    Removing the brackets allows the aphabetical ordering to be the same as
    the downloaded order for the first 10 entries with identical details
    (and feeds with more than 10 identically titled and dated episodes
    probably have other issues!).
    
    Closes #206.

--- Classes/Model/Download.cs
+++ Classes/Model/Download.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of Radio Downloader.
- * Copyright © 2007-2015 by the authors - see the AUTHORS file for details.
+ * Copyright © 2007-2016 by the authors - see the AUTHORS file for details.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -456,7 +456,7 @@ namespace RadioDld.Model
             while (Directory.GetFiles(saveDir, Path.GetFileName(savePath) + ".*").Length > 0 &&
                    savePath != currentFileName)
             {
-                savePath = rootName + " (" + Convert.ToString(diffNum, CultureInfo.CurrentCulture) + ")";
+                savePath = rootName + Convert.ToString(diffNum, CultureInfo.CurrentCulture);
                 diffNum += 1;
             }
 
