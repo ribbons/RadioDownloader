ribbons/RadioDownloader

commit 17dc36bfeb6a6b71bb0fb4059eaaaeec8bc0245e

Add CancelDownload method to provider interface
Instead of just calling Thread.Abort which is messy and doesn't let
providers clean up (for instance podcasts continue to download on a
separate thread), add a CancelDownload method to the IRadioProvider
interface which is called when a download is cancelled.

If the provider doesn't return from the main provider thread within 10
seconds, fall back to using Thread.Abort and set the download to Errored
so that the hung / blocked stack trace can be reported.

This resolves #154.

Classes/DownloadHandler.cs
@@ -1,6 +1,6 @@
 /* 
  * This file is part of Radio Downloader.
- * Copyright © 2007-2013 by the authors - see the AUTHORS file for details.
+ * Copyright © 2007-2014 by the authors - see the AUTHORS file for details.
  * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
@@ -39,6 +39,9 @@ namespace RadioDld
 
         private Thread downloadThread;
         private object downloadThreadLock = new object();
+
+        private bool cancelled;
+        private bool cancelResponse;
 
         public DownloadHandler(int epid)
         {
@@ -125,13 +128,34 @@ namespace RadioDld
             }
         }
 
-        public void Cancel()
+        public bool Cancel()
         {
             lock (this.downloadThreadLock)
             {
-                if (this.downloadThread != null && this.downloadThread.IsAlive)
+                lock (this.pluginInstanceLock)
                 {
+                    this.cancelled = true;
+
+                    if (this.downloadThread == null || !this.downloadThread.IsAlive)
+                    {
+                        return true;
+                    }
+
+                    this.pluginInstance.CancelDownload();
+
+                    for (int wait = 0; wait < 20; wait++)
+                    {
+                        Thread.Sleep(500);
+
+                        if (this.cancelResponse)
+                        {
+                            return true;
+                        }
+                    }
+
+                    // No timely response to request, kill the thread
                     this.downloadThread.Abort();
+                    return false;
                 }
             }
         }
@@ -198,11 +222,6 @@ namespace RadioDld
 
                 fileExtension = this.pluginInstance.DownloadProgramme(this.progExtId, this.episodeExtId, this.providerProgInfo, this.providerEpisodeInfo, finalName);
             }
-            catch (ThreadAbortException)
-            {
-                // The download has been cancelled
-                return;
-            }
             catch (DownloadException downloadExp)
             {
                 if (downloadExp.ErrorType == ErrorType.UnknownError)
@@ -220,6 +239,11 @@ namespace RadioDld
             {
                 this.DownloadError(unknownExp);
                 return;
+            }
+
+            if (this.cancelled)
+            {
+                this.cancelResponse = true;
             }
 
             finalName += "." + fileExtension;

Classes/DownloadManager.cs
@@ -1,6 +1,6 @@
 ﻿/* 
  * This file is part of Radio Downloader.
- * Copyright © 2007-2012 by the authors - see the AUTHORS file for details.
+ * Copyright © 2007-2014 by the authors - see the AUTHORS file for details.
  * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
@@ -59,27 +59,22 @@ namespace RadioDld
             StartNextDownload();
         }
 
-        public static void CancelDownload(int epid)
+        public static bool CancelDownload(int epid)
         {
-            bool cancelled = false;
-
             lock (downloads)
             {
-                if (downloads.ContainsKey(epid))
+                if (!downloads[epid].Cancel())
                 {
-                    downloads[epid].Cancel();
-                    cancelled = true;
-
-                    downloads.Remove(epid);
-                    startedDownloads.Remove(epid);
+                    return false;
                 }
+
+                downloads.Remove(epid);
+                startedDownloads.Remove(epid);
             }
 
-            if (cancelled)
-            {
-                UpdateTotalProgress();
-                StartNextDownload();
-            }
+            UpdateTotalProgress();
+            StartNextDownload();
+            return true;
         }
 
         private static void ResumeDownloadsAsync()

Classes/Model/Download.cs
@@ -850,7 +850,10 @@ namespace RadioDld.Model
         {
             if (!auto)
             {
-                DownloadManager.CancelDownload(epid);
+                if (!DownloadManager.CancelDownload(epid))
+                {
+                    return;
+                }
             }
 
             lock (Database.DbUpdateLock)

Interfaces/IRadioProvider.cs
@@ -1,6 +1,6 @@
 ﻿/* 
  * This file is part of Radio Downloader.
- * Copyright © 2007-2012 by the authors - see the AUTHORS file for details.
+ * Copyright © 2007-2014 by the authors - see the AUTHORS file for details.
  * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
@@ -89,6 +89,11 @@ namespace RadioDld
         /// <exception cref="DownloadException">Thrown when an expected error is encountered whilst downloading.</exception>
         /// <returns>The file extension of a successful download.</returns>
         string DownloadProgramme(string progExtId, string episodeExtId, ProgrammeInfo progInfo, EpisodeInfo epInfo, string finalName);
+
+        /// <summary>
+        /// Cancel the episode download currently in progress.
+        /// </summary>
+        void CancelDownload();
     }
 
     /// <summary>

Providers/PodcastProvider/Classes/DownloadWrapper.cs
@@ -1,6 +1,6 @@
 /* 
  * This file is part of the Podcast Provider for Radio Downloader.
- * Copyright © 2007-2013 by the authors - see the AUTHORS file for details.
+ * Copyright © 2007-2014 by the authors - see the AUTHORS file for details.
  * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
@@ -27,9 +27,6 @@ namespace PodcastProvider
 
         private Uri downloadUrl;
         private string destPath;
-        private bool downloadComplete;
-
-        private Exception downloadError;
 
         public DownloadWrapper(Uri downloadUrl, string destPath)
         {
@@ -41,15 +38,11 @@ namespace PodcastProvider
 
         public event DownloadProgressEventHandler DownloadProgress;
 
-        public bool Complete
-        {
-            get { return this.downloadComplete; }
-        }
+        public bool Complete { get; private set; }
 
-        public Exception Error
-        {
-            get { return this.downloadError; }
-        }
+        public Exception Error { get; private set; }
+
+        public bool Cancelled { get; private set; }
 
         public void Download()
         {
@@ -63,6 +56,12 @@ namespace PodcastProvider
             this.downloadClient.DownloadFileAsync(this.downloadUrl, this.destPath);
         }
 
+        public void Cancel()
+        {
+            this.Cancelled = true;
+            this.downloadClient.CancelAsync();
+        }
+
         private void DownloadClient_DownloadProgressChanged(object sender, System.Net.DownloadProgressChangedEventArgs e)
         {
             if (this.DownloadProgress != null)
@@ -73,18 +72,21 @@ namespace PodcastProvider
 
         private void DownloadClient_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
         {
-            if (!e.Cancelled)
+            if (e.Cancelled && !this.Cancelled)
             {
-                SystemEvents.PowerModeChanged -= this.PowerModeChange;
+                // Cancelled before retry
+                return;
+            }
 
-                if (e.Error != null)
-                {
-                    this.downloadError = e.Error;
-                }
-                else
-                {
-                    this.downloadComplete = true;
-                }
+            SystemEvents.PowerModeChanged -= this.PowerModeChange;
+
+            if (e.Error != null)
+            {
+                this.Error = e.Error;
+            }
+            else if (!this.Cancelled)
+            {
+                this.Complete = true;
             }
         }
 

Providers/PodcastProvider/Classes/PodcastProvider.cs
@@ -393,6 +393,11 @@ namespace PodcastProvider
                 while ((!this.doDownload.Complete) && this.doDownload.Error == null)
                 {
                     Thread.Sleep(500);
+
+                    if (this.doDownload.Cancelled)
+                    {
+                        return null;
+                    }
                 }
 
                 if (this.doDownload.Error != null)
@@ -431,6 +436,11 @@ namespace PodcastProvider
             }
 
             return extension;
+        }
+
+        public void CancelDownload()
+        {
+            this.doDownload.Cancel();
         }
 
         internal void RaiseFindNewException(Exception exception)

commit bac7dcddc6af639391c038c3a5de84a298f42059

Don't treat a cancelled download as completed
Add a return statement after handling the cancellation of a download so
that it isn't treated as completed before deletion.  This fixes a crash
trying to build a final filename.

The logic error was introduced in 17dc36bf (clean download cancellation)
and was promoted into a crash by d841a4dc (simple chapters handling) as
it attempts to dereference the null DownloadInfo object from the Podcast
Provider.

Resolves #225.

Classes/DownloadHandler.cs
@@ -245,6 +245,7 @@ namespace RadioDld
             if (this.cancelled)
             {
                 this.cancelResponse = true;
+                return;
             }
 
             finalName += "." + info.Extension;