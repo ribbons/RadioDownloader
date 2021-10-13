b5ec7b6d77d096b3487b1715d3fd1fe035ce2c97
Matt Robinson <git@nerdoftheherd.com>
2021-10-13 20:48:50 +0100

    Move author copyright to individual files
    Instead of trying to handle the list of authors separately to the file
    copyright, remove AUTHORS.md and change the copyright in all the files
    to the contributor names (this effectively reverts e222497235).  This
    helps to keep the author copyright with the file content if any of the
    files are used elsewhere.
    
    While updating the copyright headers, also correct the 'from' copyright
    date in a number of files which was incorrectly set to 2007 in all files
    in 7e0a09f069.
    
    Also take the opportunity to reduce the size of the file headers by
    removing the GPL boilerplate from each file and replacing it with a
    SPDX-License-Identifier header line.

--- tools/recoverdb/publish.bat
+++ tools/recoverdb/publish.bat
@@ -1,20 +1,8 @@
 @echo off
 
-rem This file is part of Radio Downloader.
-rem Copyright © 2007-2014 by the authors - see the AUTHORS file for details.
+rem Copyright © 2014 Matt Robinson
 rem
-rem This program is free software: you can redistribute it and/or modify
-rem it under the terms of the GNU General Public License as published by
-rem the Free Software Foundation, either version 3 of the License, or
-rem (at your option) any later version.
-rem
-rem This program is distributed in the hope that it will be useful,
-rem but WITHOUT ANY WARRANTY; without even the implied warranty of
-rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-rem GNU General Public License for more details.
-rem
-rem You should have received a copy of the GNU General Public License
-rem along with this program.  If not, see <http://www.gnu.org/licenses/>.
+rem SPDX-License-Identifier: GPL-3.0-or-later
 
 rem Load any user / machine specific variable values
 if exist ../../build/publish-win-vars.bat call ../../build/publish-win-vars.bat
